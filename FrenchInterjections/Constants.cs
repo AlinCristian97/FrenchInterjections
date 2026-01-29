895ec572eea348dbd9bdb1d62a329583b48abed4
Alin <alin.cristian977@gmail.com>
2026-01-30 00:46:11 +0100

    Create Constants.cs

---
+++ FrenchInterjections/Constants.cs
@@ -0,0 +1,37 @@
+﻿namespace FrenchInterjections;
+
+public class Constants
+{
+    public static class Categories
+    {
+        public const string Greetings = "Greetings";
+        public const string Encouragement = "Encouragement";
+        public const string Warnings = "Warnings";
+        public const string Celebratory = "Celebratory";
+        public const string Action = "Action";
+    }
+
+    public static class Interjections
+    {
+        // Greetings
+        public const string Salut = "Salut !";
+        public const string Bonjour = "Bonjour !";
+        public const string Bonsoir = "Bonsoir !";
+
+        // Encouragement
+        public const string Bravo = "Bravo !";
+        public const string BienJoue = "Bien joué !";
+
+        // Warnings
+        public const string Attention = "Attention !";
+        public const string Gare = "Gare !";
+
+        // Celebratory
+        public const string Bienvenue = "Bienvenue !";
+        public const string Felicitations = "Félicitations !";
+
+        // Action / Start
+        public const string CestParti = "C’est parti !";
+        public const string AllonsY = "Allons-y !";
+    }
+}
